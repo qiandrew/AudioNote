a6cb421d6f0b1be44b508353c93646adf0d1d7c6
Matt Moss <matthewmoss@me.com>
2019-04-16 01:25:00 -0700

    basic requests working

---
+++ Backend/src/main/java/audionote/Application.java
@@ -0,0 +1,15 @@
+package audionote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    // This is the main method which gets called by Maven when the application starts up.
+    // All we have to do is start Spring.
+    public static void main(String[] args) {
+        // Start Spring application
+        SpringApplication.run(Application.class, args);
+    }
+}
