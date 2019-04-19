aa9f89a1dfcb8dbb95adfb4324633feca0204331
qiandrew <andrew99qi@gmail.com>
2019-04-19 16:34:07 -0700

    basic test

--- Backend/src/main/java/audionote/Analysis.java
+++ Backend/src/main/java/audionote/Analysis.java
@@ -15,11 +15,16 @@ public class AnalysisPackage{
     public AnalysisPackage(String s){
         String sf = format(s);
     }
-    public incrementWord(String s) {
+    public void incrementWord(String s) {
+        return;
         //Adds a singular word or increases its count by 1.
     }
-    public void addWords(String s);//Adds a String that has more than one word.
-    public String format(String s);//Formats into all lowercase, no special characters.
+    public void addWords(String s){
+        return;
+    }//Adds a String that has more than one word.
+    public String format(String s){
+        return "-";
+    }//Formats into all lowercase, no special characters.
 
     private Arraylist<Pair<String, Integer>> wordListNumSort;
     private ArrayList<Pair<String, Integer>> wordListAlphaSort;
