8030f936c4e631ba649df82b28795a2e238bcf03
qiandrew <andrew99qi@gmail.com>
2019-04-22 14:45:59 -0700

    analysis package almost done

--- Backend/src/main/java/audionote/AnalysisPackage.java
+++ Backend/src/main/java/audionote/AnalysisPackage.java
@@ -12,21 +12,48 @@ public class AnalysisPackage{
     public AnalysisPackage(){
         wordNum = 0;
     }
-    public AnalysisPackage(String s){
-        String sf = format(s);
+    public AnalysisPackage(String str){
+        s = str;
+        sf = format(str);
+        addWords(sf);
     }
-    public void incrementWord(String s) {
+    public void incrementWord(String str) {
+        //for(int i = 0; i < )
         return;
         //Adds a singular word or increases its count by 1.
     }
-    public void addWords(String s){
-        return;
+    public void addWords(String str){
+        String currWord;
+        for(int i = 0; i < str.length(); i++){
+            if(str[i] == 32){
+                if(currWord != ""){
+                    incrementWord(currWord);
+                }
+                currWord = "";
+            }
+            else{
+                currWord += str[i];
+            }
+        }
     }//Adds a String that has more than one word.
-    public String format(String s){
-        return "-";
+
+    public String format(String str){
+        String ret;
+        for(int i = 0; i < s.legnth; i++){
+            if(str[i] == 32 || (str[i] >= 97 && str[i] <= 122)){
+                ret += str[i];
+            }
+            else if(str[i] >= 65 && str[i] <= 90){
+                ret += (str[i] + 32)
+            }
+        }
+        return ret;
     }//Formats into all lowercase, no special characters.
 
-    //private Arraylist<Pair<String, Integer>> wordListNumSort;
-    //private ArrayList<Pair<String, Integer>> wordListAlphaSort;
+    private Arraylist<Pair<String, Integer>> wordListNumSort;
+    private ArrayList<Pair<String, Integer>> wordListAlphaSort;
     private int wordNum;
+    private string s;
+    private string sf;
+
 }
