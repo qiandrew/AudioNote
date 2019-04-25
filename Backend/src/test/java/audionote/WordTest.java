96b69e25f2c85ff5f4b4a14c2895776466ae7743
qiandrew <andrew99qi@gmail.com>
2019-04-25 15:06:30 -0700

    testing

--- Backend/src/test/java/audionote/WordTest.java
+++ Backend/src/test/java/audionote/WordTest.java
@@ -1,9 +1,16 @@
 import static org.junit.Assert.*;
 import org.junit.Test;
+import audionote.Word;
 
 //comment to commit
 public class WordTest{
     @Test
-    public void testWordPass() {
+    public void testWord1() {
+        Word w = new Word("apple", 1, 0, 1);
+        assertEquals("error in word:", "apple", w.getWord());
+        assertEquals("error in confidence:", 1, w.getConfidence());
+        assertEquals("error in start:", 0, w.getStartTime());
+        assertEquals("error in end:", 1, w.getEndTime());
+        assertEquals("error in total:", 1, w.getTotalTime());
     }
 }
